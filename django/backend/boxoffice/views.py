1f2f94f63845fc09a03613e87f4548c7c9ec40e0
dseflinger <dseflinger@gmail.com>
2025-05-17 16:21:47 -0700

    added more logic for revenue_summary, some styling

--- django/backend/boxoffice/views.py
+++ django/backend/boxoffice/views.py
@@ -1,8 +1,8 @@
+from django.http import HttpResponseBadRequest
 from django.shortcuts import render
 from boxoffice.models import Movie, Theater, Sale
-from django.db.models import Sum, Max, F
-
-# Create your views here.
+from django.db.models import Sum, F
+from datetime import datetime
 
 def home(request):
     return render(request, "boxoffice/home.html")
@@ -20,37 +20,43 @@ def sales_list(request):
     return render(request, "boxoffice/sales.html", {"sales": sales})
 
 def revenue_summary(request):
-    #todo validate date field
+    # todo: should i add validation for date range? (too far in future or past)
     if request.method == 'GET':
         date = request.GET.get('date')
-        sales_by_theater = salesByTheater(date)
+        try:
+            datetime.strptime(date, "%Y-%m-%d")
+        except (ValueError, TypeError):
+            return HttpResponseBadRequest("Invalid date format")
+        
+        sales_by_date = Sale.objects.filter(date=date)
+        sales_by_theater = (
+            sales_by_date
+                .values('theater')
+                .annotate(name = F('theater__name'), total_sum=Sum('amount'), total_tickets=Sum('tickets_sold'), cost_per_ticket=Sum('amount') / Sum('tickets_sold'))
+        )
         top_theater_name = topTheaterByRevenue(sales_by_theater)
         top_theater_by_tickets_name = topTheaterByTicketsSold(sales_by_theater)
 
-        #avg cost per ticket?
-        #best selling genre
-        #best selling movie
-        #also have a return to home button
-        #maybe return an aggregation by theater - theater name, total amount sold that day, total tickets sold that day, price per ticket? 
-        return render(request, "boxoffice/revenue_summary.html", 
-                      {"date": date, "sales_by_theater" : sales_by_theater, "top_theater": top_theater_name, "top_theater_by_tickets": top_theater_by_tickets_name})
-
-def salesByTheater(date):
-    sales_by_theater = (
-            Sale.objects
-                .filter(date=date)
-                .values('theater')
-                .annotate(name = F('theater__name'), total_sum=Sum('amount'), total_tickets=Sum('tickets_sold'))
+        sales_by_movies = (
+            sales_by_date
+                .values('movie')
+                .annotate(name = F('movie__name'), genre=F('movie__genre'), total_sum=Sum('amount'))
         )
-    
-    return sales_by_theater
+        best_selling_movie = sales_by_movies.order_by("-total_sum").first()
+
+        return render(request, "boxoffice/revenue_summary.html", 
+                      {"date": date, 
+                       "sales_by_theater" : sales_by_theater, 
+                       "top_theater": top_theater_name, 
+                       "top_theater_by_tickets": top_theater_by_tickets_name, 
+                       "best_selling_movie": best_selling_movie})
 
 def topTheaterByTicketsSold(sales_by_theater):
     top_theater_by_tickets = sales_by_theater.order_by("-total_tickets").first()
-    top_theater_by_tickets_name = top_theater_by_tickets['name'] if top_theater_by_tickets else None
+    top_theater_by_tickets_name = top_theater_by_tickets['name'] if top_theater_by_tickets else ""
     return top_theater_by_tickets_name
 
 def topTheaterByRevenue(sales_by_theater):
     top_theater = sales_by_theater.order_by("-total_sum").first()
-    top_theater_name = top_theater['name'] if top_theater else None
+    top_theater_name = top_theater['name'] if top_theater else  ""
     return top_theater_name
