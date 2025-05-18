9427c086de70c800acce5af4fcff6cae6a481f59
dseflinger <dseflinger@gmail.com>
2025-05-17 18:17:03 -0700

    added inital setup for boxoffice cli project

---
+++ .net cli app/boxoffice/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Npgsql;
+
+// Console.WriteLine("Enter a date: ");
+// var dateString = Console.ReadLine();
+
+// if (DateTime.TryParse(dateString, out DateTime date))
+// {
+//     // todo handle logic
+// }
+// else
+// {
+
+//     Console.WriteLine("Please Enter a Valid Date");
+// }
+
+var connectionString = "Host=localhost;Username=postgres;Password=danPost4247?;Database=boxoffice_db";
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+await using var connection = await dataSource.OpenConnectionAsync(); // might be able to skip this by connnecting to string directly
+
+
+await using var command = new NpgsqlCommand("SELECT name FROM boxoffice_movie ORDER BY id ASC", connection);
+await using var reader = await command.ExecuteReaderAsync();
+
+while (await reader.ReadAsync())
+{
+    Console.WriteLine(reader.GetString(0));
+}
+
+Console.WriteLine("didnt think it'd get this far did you???");
+
+
+// todo create connection string and query db
+// maybe add keyboard things to leave if not built in?
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-This is a project for analyzing box office performance, using two programs, Django and Next.js.
+This is a project for analyzing box office performance, using two programs, Django and .NET CLI
 <!-- todo add a requirements.txt -->
 <!-- todo describe how to use load_data -->
